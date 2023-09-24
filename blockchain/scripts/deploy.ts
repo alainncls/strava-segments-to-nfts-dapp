9c18df919bda714b14e2e5b0c2b3b7fd761adfbe
Alain Nicolas <alainncls@users.noreply.github.com>
2023-09-24 23:45:48 +0200

    ✨ Generate a file to map networks and contract addresses (#34)

--- blockchain/scripts/deploy.ts
+++ blockchain/scripts/deploy.ts
@@ -1,4 +1,18 @@
-import { ethers, run } from "hardhat";
+import { ethers, network, run } from "hardhat";
+import * as stravaSegmentJson from "../artifacts/contracts/StravaSegment.sol/StravaSegment.json";
+import { networks } from "../../www/src/config/StravaSegment.json";
+import { writeFile } from "node:fs";
+import { Buffer } from "node:buffer";
+
+interface Config {
+  chainId: string;
+  abi: any;
+  address: string;
+}
+
+interface NetworkConfig {
+  networks: Config[];
+}
 
 async function main() {
   console.log("Deploying StravaSegment...");
@@ -14,8 +28,46 @@ async function main() {
   });
 
   console.log(
-    `StravaSegment successfully deployed and verified at ${stravaSegmentAddress}!`,
+    `StravaSegment contract successfully deployed and verified at ${stravaSegmentAddress}!`,
   );
+
+  console.log(`Generating config file...`);
+
+  const chainIdHex = await network.provider.send("eth_chainId");
+  const chainId = BigInt(chainIdHex).toString();
+
+  if (!stravaSegmentJson.abi) {
+    throw new Error("Something went wrong");
+  }
+
+  const newConfig: Config = {
+    chainId,
+    address: stravaSegmentAddress,
+    abi: stravaSegmentJson.abi,
+  };
+
+  const index = networks.findIndex(
+    (network) => network.chainId === newConfig.chainId,
+  );
+
+  if (index < 0) {
+    networks[networks.length] = newConfig;
+  } else {
+    networks[index] = newConfig;
+  }
+
+  const frontendConfig: NetworkConfig = { networks };
+
+  const data = new Uint8Array(
+    Buffer.from(JSON.stringify(frontendConfig, null, 2)),
+  );
+
+  writeFile("../www/src/config/StravaSegment.json", data, (err) => {
+    if (err) throw err;
+    console.log("The file has been saved!");
+  });
+
+  console.log(`Config file generated!`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
