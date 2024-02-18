610c1d33ccd06f50606eff78311dc55b85d8204d
Alain Nicolas <alainncls@users.noreply.github.com>
2024-02-18 15:58:44 +0100

    ✨ Use Netlify CLI (#43)

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 3000,
+    port: 5173,
   },
   base: "/",
   plugins: [react()],
